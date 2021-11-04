573fa9868a767029720bf729acbd9d1b3bbae4fb
Doroteo Corona <92071824+DoroteoCorona@users.noreply.github.com>
2021-11-03 20:27:46 -0600

    Agregar archivos de proyecto.

---
+++ SaferManagementSystem/Controllers/InicioAdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaferManagementSystem.Controllers
+{
+    public class InicioAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult InicioAdmin()
+        {
+            return View();
+        }
+    }
+
+    
+}
+
+
